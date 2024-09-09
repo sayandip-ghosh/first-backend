e105b73d7e932066f3ab09cf9c8db80a5c66951c
sayandip-ghosh <mrsayandip@gmail.com>
2024-09-09 21:20:03 +0530

    user and video model added

---
+++ src/models/user.model.js
@@ -0,0 +1,86 @@
+import mongoose, { Schema } from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+const userSchema = new Schema({
+    username:{
+        type:String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        index: true
+    },
+    email:{
+        type:String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    fullname:{
+        type:String,
+        required: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    avatar:{
+        type:String,
+        required: true
+    },
+    coverImage:{
+        type:String,
+        required: true
+    },
+    watchHistory:{
+        type: Schema.Types.ObjectId,
+        ref: 'Video'
+    },
+    password:{
+        type:String,
+        required: [true, 'Password is required']
+    },
+    refreshToken:{
+        type:String
+    }
+},{timestamps: true});
+
+userSchema.pre('save', async function(next){
+    if(this.isModified('password')){
+        this.password = await bcrypt.hash(this.password, 8);
+    }
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model('User', userSchema);
